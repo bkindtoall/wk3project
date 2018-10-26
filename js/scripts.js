46c4a5e5dd4495597ce86064c365e876f1e2f008
Edin Filipovic <edin.filipovic89@gmail.com>
2018-10-26 16:29:50 -0700

    first beep if works, onto next

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,26 @@
 $(document).ready(function(){
   $("form#formOne").submit(function(event){
     event.preventDefault();
-    var number = parseInt($("input#userNumber").val());
 
-  var arrayNum = number;
+  var number = parseInt($("input#userNumber").val());
   var countArray = [];
   var count = 0;
 
+  for (var i = 0; i < number; i++) {
+    count ++;
+    countArray.push(count);
+    console.log(countArray)
+  }
+    //got a listed count up to and including input number
+      var beep = [];
+  for (var i = 0; i < countArray.length; i++) {
+    var toString = (countArray[i]).toString();
+    if (toString.includes(0) === true) {
+      beep.push("Beep!")
+      console.log(beep);
+    };
+  }
 
-    for (var i = 0; i < number; i++) {
-      count ++;
-      countArray.push(count);
-      console.log(countArray)
-    }
 
 
 
