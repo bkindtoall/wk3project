cffe539aa0bc474a1503054a95d087a140793aa2
Edin Filipovic <edin.filipovic89@gmail.com>
2018-10-28 17:05:37 -0700

    finished boop and dave else ifs, merged the 3 arrays with concat

--- js/scripts.js
+++ js/scripts.js
@@ -13,24 +13,27 @@ $(document).ready(function(){
     countArray.push(count);
     console.log(countArray)
   }
-    //got a listed count up to and including input number
-      var beep = [];
-      var boop = [];
+  //got a listed count up to and including input number
+  var beep = [];
+  var boop = [];
+  var dave = [];
+
   for (var i = 0; i < countArray.length; i++) {
     var toString = (countArray[i]).toString();
     if (toString.includes(0) === true) {
       beep.push("Beep!")
       console.log(beep);
-    // } else if (toString.includes(1) === true);
-    //   boop.push("Boop!")
-    //   console.log(boop);
-  };
-};
-
-
-
-
-
-// formOne(userNumber);
+    }
+      else if (toString.includes(1) === true) {
+        boop.push("Boop!")
+        console.log(boop);
+      }
+      else if (countArray[i]%3 === 0) {
+        dave.push("Sorry Dave");
+        console.log(dave);
+      }
+    };
+    var mergedArrays = beep.concat(boop, dave);
+    console.log(mergedArrays)
   });
 });
