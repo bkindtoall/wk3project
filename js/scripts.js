74fecd48c023fd2bc89f3e123cf0a0ce4e6d4d5c
Edin Filipovic <edin.filipovic89@gmail.com>
2018-10-31 02:43:10 -0700

    redid the back end

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,29 @@
 // @flow
 
+function BeepBoop(input) {
+  var results = [];
+  for (var i = 0; i <= input; i++) {
+    if (i%3 == 0) {
+      results.push("I'm sorry Dave. I'm afraid I can't do that.");
+      console.log(results);
+    } else if (i.toString().match(/0/g)) {
+      results.push("Beep!")
+    } else if (i.toString().match(/1/g)) {
+      results.push("Boop!")
+    } else {
+      results.push(i);
+    }
+  }
+  return results;
+}
+
+
+//user interface
 $(document).ready(function(){
   $("form#formOne").submit(function(event){
     event.preventDefault();
 
-  var number = parseInt($("input#userNumber").val());
-  var countArray = [];
-  var count = 0;
-
-  for (var i = 0; i < number; i++) {
-    count ++;
-    countArray.push(count);
-    // console.log(countArray)
-  }
-  //got a listed count up to and including input number
-  var beep = [];
-
-
-  for (var i = 0; i < countArray.length; i++) {
-    var toString = (countArray[i]).toString();
-    if (toString.includes(0) === true) {
-      beep[i] = "Beep!";
-      // console.log(beep);
-    }
-      else if (toString.includes(1) === true) {
-        beep[i] = "Boop!";
-        // console.log(beep);
-      }
-      else if (i>0 && i%3==0) {
-        beep[i] = "Sorry Dave"
-        // console.log(beep);
-      }
-    };
-    // var mergedArrays = beep.concat(boop, dave);
-    // console.log(mergedArrays);
-    var masterMerged = beep.concat(countArray);
-    console.log(masterMerged)
+  var input = parseInt($("input#userNumber").val());
+  $("#output").text(BeepBoop(input));
   });
-  function output(masterMerged) {
-     return masterMerged
-  }
-  // $("#output").text(masterMerged);
-  //   return masterMerged;
 });
