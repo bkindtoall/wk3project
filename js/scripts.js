1019b161dfa88c9913fdaa07c255513367ed09fb
Edin Filipovic <edin.filipovic89@gmail.com>
2018-10-31 03:33:40 -0700

    tweeked styling and checked code org

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,30 @@
-// @flow
-
+//behind the scenes
 function BeepBoop(input) {
-  var results = [];
-  for (var i = 0; i <= input; i++) {
-    if (i%3 == 0) {
-      results.push("I'm sorry Dave. I'm afraid I can't do that.");
-      console.log(results);
-    } else if (i.toString().match(/0/g)) {
-      results.push("Beep!")
-    } else if (i.toString().match(/1/g)) {
-      results.push("Boop!")
-    } else {
-      results.push(i);
-    }
+var results = [];
+for (var i = 0; i <= input; i++) {
+  if (i%3 == 0) {
+    results.push("I'm sorry Dave. I'm afraid I can't do that.");
+    console.log(results);
+  } else if (i.toString().match(/0/g)) {
+    results.push("Beep!")
+  } else if (i.toString().match(/1/g)) {
+    results.push("Boop!")
+  } else {
+    results.push(i);
   }
-  return results;
+}
+return results;
 }
 
 
 //user interface
 $(document).ready(function(){
-  $("form#formOne").submit(function(event){
-    event.preventDefault();
+$("form#formOne").submit(function(event){
+  event.preventDefault();
 
-  var input = parseInt($("input#userNumber").val());
-  $("input#userNumber").val("");
-  $("#output").text(BeepBoop(input));
-  $("h3").show();
-  });
+var input = parseInt($("input#userNumber").val());
+$("input#userNumber").val("");
+$("#output").text(BeepBoop(input));
+$("h3").show();
+});
 });
